5e2de5ee5bca0a7696d1194940c5769277ae362a
BILL VELIZ <bvelizmera@gmail.com>
2024-03-03 23:25:43 +0000

    ready for testing

--- about/models.py
+++ about/models.py
@@ -6,10 +6,9 @@ from cloudinary.models import CloudinaryField
 
 class About(models.Model):
 
-     """
+    """
     Stores a single about me text
     """
-
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
